311434ede143ad0cae856708356c26562201de1d
Vaibhav Bhardwaj <vaibhavbhardwaj3010@gmail.com>
2025-09-09 21:58:23 +0530

    color buttton functionality

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 let currentColor = 'black'; // default color
+//colour button code
+const colorButton = document.getElementById('colorButton');
+const colorPicker = document.getElementById('colorPicker');
+
+colorButton.addEventListener('click', () => {
+  colorPicker.click(); // opens native color picker
+});
+
+colorPicker.addEventListener('input', () => {
+  currentColor = colorPicker.value; // updates your drawing color
+});
+
+//slider and grid code
 function createGrid(size) {
   const mainBox = document.getElementById('MainBox');
   mainBox.innerHTML = '';
@@ -7,20 +20,24 @@ function createGrid(size) {
   mainBox.style.width = '500px';
   mainBox.style.height = '500px';
 
-
-
   for (let i = 0; i < size; i++) {
     const row = document.createElement('div');
     row.style.display = 'flex';
     row.style.flex = '1';
 
     for (let j = 0; j < size; j++) {
+      let isDrawing = false;
       const box = document.createElement('div');
       box.style.flex = '1';
       box.style.border = '1px solid #ccc';
       box.style.backgroundColor = 'white';
-      box.addEventListener('mouseover', () => {
-        box.style.backgroundColor = currentColor;
+      document.addEventListener('mousedown', () => isDrawing = true);
+      document.addEventListener('mouseup', () => isDrawing = false);
+
+      box.addEventListener('mousemove', () => {
+      if (isDrawing) {
+      box.style.backgroundColor = currentColor === 'rainbow' ? getRandomColor() : currentColor;
+       }
       });
       row.appendChild(box);
     }
@@ -39,7 +56,7 @@ document.getElementById('rainbowButton').addEventListener('click', () => {
   });
 });
 });
-
+// rainbow button code
 function getRandomColor() {
   const r = Math.floor(Math.random() * 256);
   const g = Math.floor(Math.random() * 256);
